8249b06ee7e0707766267e6afb443c36be14ff8d
Jon-Taylor <jonathan.taylor@esss.se>
2017-10-24 22:16:06 +0200

    a sanbox

--- sandbox.py
+++ sandbox.py
@@ -28,4 +28,20 @@ print(groups)
 print(devices)
 
 print(devices)
+lightgroup=[]
+for groupid in groups:
+    lightgroup.append(tradfriStatus.tradfri_get_group(hubip, securityid,
+                                                      str(groupid)))
+def allOn(groups):
+    for group in groups:
+        tradfri_dim_group(hubip, securityid, group, 100)
 
+
+def tradfri_dim_group(hubip, securityid, groupid, value):
+    """ function for dimming tradfri lightbulb """
+    tradfriHub = 'coaps://{}:5684/15004/{}'.format(hubip, groupid)
+    dim = float(value) * 2.55
+    payload = '{ "5851" : %s }' % int(dim)
+
+    api = '{} -m put -u "Client_identity" -k "{}" -e \'{}\' "{}"'.format(coap, securityid,payload, tradfriHub)
+    result = os.popen(api)
